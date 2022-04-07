0c1363552fa10dde7a737213f44e29946af2895a
Eglenelid Gamaliel Gutierrez Hernandez <eglenelid.gamaliel@gmail.com>
2022-04-07 15:23:35 -0500

    Quickfix

--- src/window.py
+++ src/window.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pathlib import Path
 import time
+from pathlib import Path
 
 from gi.repository import Adw, Gio, GLib, Gtk
 
